2b0264d85a890d9b663c792e3b975a119457ae06
Corey Walsh <matthias61984@hotmail.com>
2018-12-17 17:47:31 -0800

    scrape success

--- server.js
+++ server.js
@@ -14,6 +14,29 @@ app.use(express.static("public"));
 var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/mongoHeadlines";
 mongoose.connect(MONGODB_URI), { useNewUrlParser: true };
 
+app.get("/scrape", function(req, res) {
+    axios.get("http://www.nytimes.com/").then(function(response) {
+      var $ = cheerio.load(response.data);
+      $("article div div a div").each(function(i, element) {
+        var result = {};
+        result.title = $(this)
+          .children("h2")
+          .text();
+        result.link = $(this)
+          .parent()
+          .attr("href");
+        db.Article.create(result)
+          .then(function(dbArticle) {
+            console.log(dbArticle);
+          })
+          .catch(function(err) {
+            console.log(err);
+          });
+      });
+      res.send("Scrape Complete");
+    });
+  });
+
 app.listen(PORT, function () {
     console.log("NYT Scraper is running on " + PORT + " !");
 })
