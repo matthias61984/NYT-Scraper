deadfeb2058d7953abaf37f402109831b770822b
Corey Walsh <matthias61984@hotmail.com>
2018-12-17 16:22:25 -0800

    connection

--- server.js
+++ server.js
@@ -11,8 +11,9 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static("public"));
 
-mongoose.connect("mongodb://localhost/mongoHeadlines", { useNewUrlParser: true });
+var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/mongoHeadlines";
+mongoose.connect(MONGODB_URI), { useNewUrlParser: true };
 
-app.listen(PORT, function() {
+app.listen(PORT, function () {
     console.log("NYT Scraper is running on " + PORT + " !");
 })
